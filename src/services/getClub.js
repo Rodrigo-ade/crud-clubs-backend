8ef2c91071651d28687801504259eee68cfbfac7
Rodrigo <98775764+Rodrigo-ade@users.noreply.github.com>
2023-09-01 07:46:13 -0300

    Fix error content
    Our errors body will have a message

--- src/services/getClub.js
+++ src/services/getClub.js
@@ -4,7 +4,7 @@ import { TEAMS_PATH } from './getClubs.js';
 
 export default function getClub(res, clubTla) {
   if (!isClubTlaValid(clubTla)) {
-    return res.status(400).json({ error: `invalid club (${clubTla}) - must have 3 capital letters` });
+    return res.status(400).json({ message: `invalid club (${clubTla}) - must have 3 capital letters` });
   }
   try {
     const clubs = JSON.parse(fs.readFileSync(TEAMS_PATH));
@@ -12,7 +12,7 @@ export default function getClub(res, clubTla) {
     if (foundClub) {
       return res.status(200).json(foundClub);
     }
-    return res.status(404).json({ error: `${clubTla} not in database` });
+    return res.status(404).json({ message: `${clubTla} not in database` });
   } catch (err) {
     return res.status(500).send(err);
   }
