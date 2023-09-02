206c5581b4716b52ceb84b8109a6371c8f73728d
Rodrigo <98775764+Rodrigo-ade@users.noreply.github.com>
2023-09-02 09:58:03 -0300

    Refactor getClub
    Because the service was called with a wrong parameter (now it's not)

--- src/routes/handlers/index.js
+++ src/routes/handlers/index.js
@@ -3,6 +3,7 @@ import getClubService from '../../services/getClub.js';
 import deleteClubService from '../../services/deleteClub.js';
 import createClubService from '../../services/createClub.js';
 import updateClubService from '../../services/updateClub.js';
+import { isClubTlaValid } from '../../utilities/utilities.js';
 
 export function getClubs(req, res) {
   getClubsService(res);
@@ -10,7 +11,10 @@ export function getClubs(req, res) {
 
 export function getClub(req, res) {
   const { clubTla } = req.params;
-  getClubService(res, clubTla);
+  if (!isClubTlaValid(clubTla)) {
+    return res.status(400).json({ message: `invalid club (${clubTla}) - must have 3 capital letters` });
+  }
+  return getClubService(res, clubTla);
 }
 
 export async function deleteClub(req, res) {
--- src/services/getClub.js
+++ src/services/getClub.js
@@ -1,11 +1,7 @@
 import fs from 'fs';
-import { isClubTlaValid } from '../utilities/utilities.js';
 import { TEAMS_PATH } from './getClubs.js';
 
 export default function getClub(res, clubTla) {
-  if (!isClubTlaValid(clubTla)) {
-    return res.status(400).json({ message: `invalid club (${clubTla}) - must have 3 capital letters` });
-  }
   try {
     const clubs = JSON.parse(fs.readFileSync(TEAMS_PATH));
     const foundClub = clubs.find((club) => club.tla === clubTla);
