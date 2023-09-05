9abbdcc999f654d4fed4a2e8a885bddbe4821700
Rodrigo <98775764+Rodrigo-ade@users.noreply.github.com>
2023-09-05 12:34:20 -0300

    Add cors dependency

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import express from 'express';
+import cors from 'cors';
 import methodOverride from 'method-override';
 import router from './routes/index.js';
 
 const app = express();
 const PORT = 8080;
+app.use(cors());
 app.use(methodOverride('_method'));
 app.use(router);
 
