f776c003382f7e0868e3bab4be2fed3741a9ffb8
Rodrigo <98775764+Rodrigo-ade@users.noreply.github.com>
2023-09-08 08:31:06 -0300

    Fix route name

--- src/routes/index.js
+++ src/routes/index.js
@@ -18,6 +18,6 @@ router.get('/clubs/:clubTla', getClub);
 router.delete('/clubs/:clubTla', deleteClub);
 router.post('/clubs', upload.single('logo_file'), createClub);
 router.put('/clubs/:clubTla', upload.single('logo_file'), updateClub);
-router.post('/clubs/update', resetClubs);
+router.post('/clubs/restore', resetClubs);
 
 export default router;
